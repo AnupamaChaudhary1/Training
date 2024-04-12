da766cb3ced1cd92571b1406f821d5004886c011
AnupamaChaudhary1 <anupamachaudhary183@gmail.com>
2024-04-12 09:11:11 +0545

    git ignore

--- Day 7/8/NetStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ Day 7/8/NetStarter/Basics/obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed1cdbd88da095bc3c7054028f732de106c327f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5daa3f4bbffc3217ec57ed0e598e64f14ee44d4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
